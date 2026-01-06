a4f46aa4a38f0ccc64c56e1006eb7c14a7616adf
rick0ll <richardranieri09@gmail.com>
2026-01-06 14:13:31 +0100

    Project done

--- src/generate_page.py
+++ src/generate_page.py
@@ -4,7 +4,7 @@ from extract_title import extract_title
 import os
 
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, base_path):
     if not os.path.exists(dir_path_content):
         raise OSError("Path con contenuto website mancante " + dir_path_content)
 
@@ -14,13 +14,15 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
         if os.path.isfile(content_path):
             dest_path = os.path.join(dest_dir_path, content)
             dest_path = dest_path.replace(".md", ".html")
-            generate_page(content_path, template_path, dest_path)
+            generate_page(content_path, template_path, dest_path, base_path)
         else:
             new_dir_path = os.path.join(dest_dir_path, content)
-            generate_pages_recursive(content_path, template_path, new_dir_path)
+            generate_pages_recursive(
+                content_path, template_path, new_dir_path, base_path
+            )
 
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, base_path):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
 
     with open(from_path) as file:
@@ -34,6 +36,8 @@ def generate_page(from_path, template_path, dest_path):
 
     template_content = template_content.replace("{{ Title }}", title)
     template_content = template_content.replace("{{ Content }}", html_file)
+    template_content = template_content.replace('href="/', f'href="{base_path}')
+    template_content = template_content.replace('src="/', f'src="{base_path}')
 
     dir_path = os.path.dirname(dest_path)
     if not os.path.exists(dir_path):
--- src/main.py
+++ src/main.py
@@ -2,14 +2,19 @@ from copy import copy_static
 from re import template
 from generate_page import generate_pages_recursive
 import os
+import sys
 
 
 def main():
+    base_path = "/"
+    if len(sys.argv) >= 2:
+        base_path = sys.argv[1]
+
     from_path = "content"
-    dest_path = "public"
+    dest_path = "docs"
     template_path = "template.html"
     copy_static()
-    generate_pages_recursive(from_path, template_path, dest_path)
+    generate_pages_recursive(from_path, template_path, dest_path, base_path)
 
 
 main()
